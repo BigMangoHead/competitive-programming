c2bbcb580f3830e5b74485e02c8d4d884ba6632a
Isaac Mango <bigmangohead@gmail.com>
2025-03-16 23:41:16 -0400

    Template additions

---
+++ templates/base.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define ll long long
+
+void run() {
+
+}
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    for (int i=0; i < n;i++) {
+
+#ifdef LOCAL
+        clog << "Case " << i+1 << endl;
+#endif
+
+        run();
+    }
+}
